e5ebad756ed067d5c9bb211260c6c1fe59119d7e
Mathias Lohne <mathialo@ifi.uio.no>
2018-03-22 15:35:48 +0100

    add products gui

--- src/bbdebet2/gui/controllers/AdminController.java
+++ src/bbdebet2/gui/controllers/AdminController.java
@@ -5,10 +5,13 @@
 package bbdebet2.gui.controllers;
 
 import bbdebet2.gui.Main;
+import bbdebet2.gui.applets.AddProducts;
+import bbdebet2.gui.modelwrappers.ViewSale;
 import bbdebet2.kernel.Kernel;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableView;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -18,15 +21,66 @@ public class AdminController implements Initializable {
     private Kernel kernel;
 
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        kernel = Main.getKernel();
-        Main.setCurrentAdminController(this);
+
+
+    @FXML
+    public void newAddProductsWindow(ActionEvent event) {
+        AddProducts.createAndDisplayDialog();
+    }
+
+
+    private void setupSaleHistoryView() {
+
+    }
+
+
+    public void repaintSaleHistory() {
+
+    }
+
+
+    private void setupStorageView() {
+
+    }
+
+
+    public void repaintStorage() {
+
+    }
+
+
+    private void setupUserListView() {
+
+    }
+
+
+    public void repaintUserList() {
+
+    }
+
+
+    public void repaintAll() {
+        repaintSaleHistory();
+        repaintStorage();
+        repaintUserList();
     }
 
 
     @FXML
     public void logout(ActionEvent event) {
         Main.toLoginScreen();
+    }
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        kernel = Main.getKernel();
+        Main.setCurrentAdminController(this);
+        setupSaleHistoryView();
+        repaintUserList();
+        setupStorageView();
+        repaintStorage();
+        setupUserListView();
+        repaintUserList();
     }
 }
