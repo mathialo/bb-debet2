0465d0074051f27330490fb25e0eec9855d71018
Mathias Lohne <mathialo@ifi.uio.no>
2018-03-22 16:09:29 +0100

    populate admin screen, added refund button

--- src/bbdebet2/gui/controllers/AdminController.java
+++ src/bbdebet2/gui/controllers/AdminController.java
@@ -6,12 +6,17 @@ package bbdebet2.gui.controllers;
 
 import bbdebet2.gui.Main;
 import bbdebet2.gui.applets.AddProducts;
+import bbdebet2.gui.modelwrappers.ViewProduct;
 import bbdebet2.gui.modelwrappers.ViewSale;
+import bbdebet2.gui.modelwrappers.ViewUser;
 import bbdebet2.kernel.Kernel;
+import bbdebet2.kernel.datastructs.Sale;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -20,7 +25,34 @@ public class AdminController implements Initializable {
 
     private Kernel kernel;
 
-
+    @FXML
+    private TableView<ViewSale> saleHistoryView;
+    @FXML
+    private TableColumn<ViewSale, String> saleHistoryTimeCol;
+    @FXML
+    private TableColumn<ViewSale, String> saleHistoryUserCol;
+    @FXML
+    private TableColumn<ViewSale, String> saleHistoryProductCol;
+    @FXML
+    private TableColumn<ViewSale, String> saleHistoryPriceCol;
+    @FXML
+    private TableView<ViewProduct> storageView;
+    @FXML
+    private TableColumn<ViewProduct, String> storageProductCol;
+    @FXML
+    private TableColumn<ViewProduct, String> storageBuyPriceCol;
+    @FXML
+    private TableColumn<ViewProduct, String> storageSellPriceCol;
+    @FXML
+    private TableView<ViewUser> userListView;
+    @FXML
+    private TableColumn<ViewUser, String> userListIdCol;
+    @FXML
+    private TableColumn<ViewUser, String> userListUserNameCol;
+    @FXML
+    private TableColumn<ViewUser, String> userListEmailCol;
+    @FXML
+    private TableColumn<ViewUser, String> userListBalanceCol;
 
 
     @FXML
@@ -30,32 +62,62 @@ public class AdminController implements Initializable {
 
 
     private void setupSaleHistoryView() {
-
+        saleHistoryTimeCol.setCellValueFactory(
+            new PropertyValueFactory<ViewSale, String>("saleDate")
+        );
+        saleHistoryUserCol.setCellValueFactory(
+            new PropertyValueFactory<ViewSale, String>("userName")
+        );
+        saleHistoryProductCol.setCellValueFactory(
+            new PropertyValueFactory<ViewSale, String>("productName")
+        );
+        saleHistoryPriceCol.setCellValueFactory(
+            new PropertyValueFactory<ViewSale, String>("price")
+        );
     }
 
 
     public void repaintSaleHistory() {
-
+        saleHistoryView.setItems(kernel.getSalesHistory().toObservableList());
     }
 
 
     private void setupStorageView() {
-
+        storageProductCol.setCellValueFactory(
+            new PropertyValueFactory<ViewProduct, String>("productName")
+        );
+        storageBuyPriceCol.setCellValueFactory(
+            new PropertyValueFactory<ViewProduct, String>("buyPrice")
+        );
+        storageSellPriceCol.setCellValueFactory(
+            new PropertyValueFactory<ViewProduct, String>("sellPrice")
+        );
     }
 
 
     public void repaintStorage() {
-
+        storageView.setItems(kernel.getStorage().toObservableList());
     }
 
 
     private void setupUserListView() {
-
+        userListIdCol.setCellValueFactory(
+            new PropertyValueFactory<ViewUser, String>("userId")
+        );
+        userListUserNameCol.setCellValueFactory(
+            new PropertyValueFactory<ViewUser, String>("userName")
+        );
+        userListEmailCol.setCellValueFactory(
+            new PropertyValueFactory<ViewUser, String>("mail")
+        );
+        userListBalanceCol.setCellValueFactory(
+            new PropertyValueFactory<ViewUser, String>("balance")
+        );
     }
 
 
     public void repaintUserList() {
-
+        userListView.setItems(kernel.getUserList().toObservableList());
     }
 
 
@@ -63,6 +125,15 @@ public class AdminController implements Initializable {
         repaintSaleHistory();
         repaintStorage();
         repaintUserList();
+    }
+
+
+    @FXML
+    public void deleteSelectedSale() {
+        Sale sale = saleHistoryView.getSelectionModel().getSelectedItem().getSaleObject();
+        if (sale == null) return;
+        kernel.getTransactionHandler().refund(sale);
+        repaintAll();
     }
 
 
