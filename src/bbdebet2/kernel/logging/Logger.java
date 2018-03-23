6b94aba98826d8aeaec9fc8f083584b9be10ff0f
Mathias Lohne <mathialo@ifi.uio.no>
2018-03-23 11:48:09 +0100

    added multiple outputs to logger

--- src/bbdebet2/kernel/logging/Logger.java
+++ src/bbdebet2/kernel/logging/Logger.java
@@ -7,19 +7,44 @@ package bbdebet2.kernel.logging;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Logger {
 
     private FileWriter logWriter;
     private boolean terminaldump;
 
+    private List<PrintStream> outputStreams;
+
 
     public Logger(File logFile, boolean writeToTerminal) throws IOException {
         logWriter = new FileWriter(logFile, true);
-        terminaldump = writeToTerminal;
+
+        outputStreams = new LinkedList<>();
+        if (writeToTerminal) {
+            outputStreams.add(System.out);
+        }
+
         log("New startup");
+    }
+
+
+    public void addOutputStream(PrintStream outputStream) {
+        outputStreams.add(outputStream);
+    }
+
+
+    public void removeOutputStream(PrintStream outputStream) {
+        outputStreams.remove(outputStream);
+    }
+
+
+    public void removeAllOutputStreams() {
+        outputStreams.clear();
     }
 
 
@@ -34,13 +59,16 @@ public class Logger {
         } catch (IOException ignored) {
         }
 
-        if (terminaldump) System.out.println("LOG:\t" + message);
+        for (PrintStream output : outputStreams) {
+            output.println("LOG:\t" + message);
+        }
     }
 
 
     public void log(Exception exception) {
         try {
-            logWriter.write(getTimeStamp() + "New " + exception.getClass().getSimpleName() + " occurred:\n");
+            logWriter.write(
+                getTimeStamp() + "New " + exception.getClass().getSimpleName() + " occurred:\n");
             logWriter.write("  -->> " + exception.getMessage() + "\n");
             logWriter.write("BEGIN STACKTRACE\n");
             StackTraceElement[] stackTrace = exception.getStackTrace();
@@ -51,7 +79,14 @@ public class Logger {
         } catch (IOException ignored) {
         }
 
-        if (terminaldump) System.out.println("LOG:\tError: " + exception.getMessage());
+        for (PrintStream output : outputStreams) {
+            output.println("LOG:\tError: " + exception.getMessage());
+        }
+    }
+
+
+    public void log(Object object) {
+        log("Requested to log object of type " + object.getClass() + ": " + object.toString());
     }
 
 
