48595523da05d723b1fd4dcb4d15aa72b36e0a78
Mathias Lohne <mathialo@ifi.uio.no>
2018-03-30 01:41:36 +0200

    added capping to log file to avoid gigantic file sizes

--- src/bbdebet2/kernel/logging/Logger.java
+++ src/bbdebet2/kernel/logging/Logger.java
@@ -4,16 +4,23 @@
 
 package bbdebet2.kernel.logging;
 
+import bbdebet2.kernel.Kernel;
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.nio.file.Files;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Logger {
+
+    private final long MAX_FILESIZE = 100000;
+
+    private File logFile;
 
     private FileWriter logWriter;
     private boolean terminaldump;
@@ -23,6 +30,7 @@ public class Logger {
 
     public Logger(File logFile, boolean writeToTerminal) throws IOException {
         logWriter = new FileWriter(logFile, true);
+        this.logFile = logFile;
 
         outputStreams = new LinkedList<>();
         if (writeToTerminal) {
@@ -48,6 +56,34 @@ public class Logger {
     }
 
 
+    private void controlFileSize() {
+        if (logFile.length() >= MAX_FILESIZE) {
+            // Close previous file
+            try {
+                logWriter.close();
+            } catch (IOException ignored) {
+            }
+
+            // Delete previous .old-file if it exists
+            File oldFile = new File(Kernel.LOG_FILEPATH + ".old");
+            if (oldFile.exists()) oldFile.delete();
+
+            // Copy log to log.old
+            try {
+                Files.copy(logFile.toPath(), oldFile.toPath());
+            } catch (IOException ignored) {
+            }
+
+            // Create new logger and new log file
+            logFile = new File(Kernel.LOG_FILEPATH);
+            try {
+                logWriter = new FileWriter(logFile);
+            } catch (IOException e) {
+            }
+        }
+    }
+
+
     private String getTimeStamp() {
         return new SimpleDateFormat("yyyy.MM.dd HH:mm:ss").format(new Date()) + ":\t";
     }
@@ -62,6 +98,8 @@ public class Logger {
         for (PrintStream output : outputStreams) {
             output.println("LOG:\t" + message);
         }
+
+        controlFileSize();
     }
 
 
@@ -82,6 +120,8 @@ public class Logger {
         for (PrintStream output : outputStreams) {
             output.println("LOG:\tError: " + exception.getMessage());
         }
+
+        controlFileSize();
     }
 
 
