99e367d1330e2f6b1e499b5ebdb39fc47a2a9cfe
Mathias Lohne <mathialo@ifi.uio.no>
2019-04-11 15:57:47 +0200

    2.0:78: Minor bugfixes in bulk adding

--- create_documentation.sh
+++ create_documentation.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 javafiles=$(find src -name *.java)
-javadoc -cp .:lib/javax.mail.jar -d doc/ -noqualifier all -link http://docs.oracle.com/javase/8/docs/api/ -link https://docs.oracle.com/javase/8/javafx/api/ $javafiles
+javadoc -cp .:lib/javax.mail.jar:lib/poi-4.0.1.jar -d doc/ -noqualifier all -link http://docs.oracle.com/javase/8/docs/api/ -link https://docs.oracle.com/javase/8/javafx/api/ $javafiles
