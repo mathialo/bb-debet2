08cfd743272aa958d6a752ce843c5146a697f516
Mathias Lohne <mathialo@ifi.uio.no>
2019-04-12 16:58:51 +0200

    2.0:81: bugfix in senddebet2data

--- etc/bashscripts/senddebet2data.sh
+++ etc/bashscripts/senddebet2data.sh
@@ -5,4 +5,4 @@ rm -rf ~/.bbdebet2/autosave/*
 ENDSSH
 
 # Send new data
-rsync --ingore-erors --update --info=progress2 --partial -t -r ~/.bbdebet2/autosave ~/.bbdebet2/hist ~/.bbdebet2/usertransactions.csv ~/.bbdebet2/moneyinserts.csv ~/.bbdebet2/processedInserts.csv ~/.bbdebet2/saleshistory.csv ~/.bbdebet2/users.usl ~/.bbdebet2/storage.csv  bb@login.uio.no:~/.bbdebet2/ 2> /dev/null
+rsync --ignore-errors --update --info=progress2 --partial -t -r ~/.bbdebet2/autosave ~/.bbdebet2/hist ~/.bbdebet2/usertransactions.csv ~/.bbdebet2/moneyinserts.csv ~/.bbdebet2/processedInserts.csv ~/.bbdebet2/saleshistory.csv ~/.bbdebet2/users.usl ~/.bbdebet2/storage.csv  bb@login.uio.no:~/.bbdebet2/ 2> /dev/null
