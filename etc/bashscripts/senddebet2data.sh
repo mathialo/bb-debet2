4d7ad499e8b51332dc5abf45ad06b28b794180fa
Mathias Lohne <mathialo@ifi.uio.no>
2019-04-24 22:07:17 +0200

    2.1:6: Add -f/--force flag to send-/getdebet2data to overwrite even when external/internal files are newer

--- etc/bashscripts/senddebet2data.sh
+++ etc/bashscripts/senddebet2data.sh
@@ -21,4 +21,8 @@ rm -rf ~/.bbdebet2/autosave/*
 ENDSSH
 
 # Send new data
-rsync --ignore-errors --update --info=progress2 --partial -t -r ~/.bbdebet2/autosave ~/.bbdebet2/hist ~/.bbdebet2/usertransactions.csv ~/.bbdebet2/moneyinserts.csv ~/.bbdebet2/processedInserts.csv ~/.bbdebet2/saleshistory.csv ~/.bbdebet2/users.usl ~/.bbdebet2/storage.csv  bb@login.uio.no:~/.bbdebet2/ 2> /dev/null
+if [[ "$1" == "-f" ]] || [[ "$1" == "--force" ]]; then
+	rsync --ignore-errors --info=progress2 --ignore-times --times --recursive ~/.bbdebet2/autosave ~/.bbdebet2/hist ~/.bbdebet2/usertransactions.csv ~/.bbdebet2/moneyinserts.csv ~/.bbdebet2/processedInserts.csv ~/.bbdebet2/saleshistory.csv ~/.bbdebet2/users.usl ~/.bbdebet2/storage.csv  bb@login.uio.no:~/.bbdebet2/ 2> /dev/null
+else
+	rsync --ignore-errors --update --info=progress2 --partial --times --recursive ~/.bbdebet2/autosave ~/.bbdebet2/hist ~/.bbdebet2/usertransactions.csv ~/.bbdebet2/moneyinserts.csv ~/.bbdebet2/processedInserts.csv ~/.bbdebet2/saleshistory.csv ~/.bbdebet2/users.usl ~/.bbdebet2/storage.csv  bb@login.uio.no:~/.bbdebet2/ 2> /dev/null
+fi
