991fd4173fe5db68f0f2cd86da9094f9bd507b21
Mathias Lohne <foss@mathiaslohne.com>
2019-11-08 17:27:09 +0100

    2.2-beta:23: Add user stats to bbctl

--- bbctl/bbctl/user.py
+++ bbctl/bbctl/user.py
@@ -25,16 +25,19 @@ def user(args: List[str] = []):
     executor = CommandExecutor("user")
     executor.add_command(Command("list", list_, "list all users"))
     executor.add_command(Command("info", info, "get info about user"))
+    executor.add_command(Command("stats", stats, "generate statistics about users"))
 
     verify_and_run_executor(executor, args)
 
 
 def list_(args: List[str] = []):
     if is_help(args):
-        print("Lists all users on this BBDebet2 system. Run with the '-v' flag to get verbose output.")
+        print("Lists all users on this BBDebet2 system. Run user list debt to se users in debt. Run\n"
+              "with the '-v' flag to get verbose output.")
         return
 
     users = []
+    balances = []
 
     with open(userfile, "r") as f:
         skipfirst = True
@@ -45,6 +48,10 @@ def list_(args: List[str] = []):
                 continue
 
             users.append(line.split(",")[1])
+            balances.append(float(line.split(",")[3]))
+
+    if "debt" in args:
+        users = [u for u, b in zip(users, balances) if b < 0]
 
     if "-v" in args or "--verbose" in args:
         info(users)
@@ -95,3 +102,35 @@ def info(args: List[str] = []):
                 print("  Email: {}".format(info[2]))
                 print("  Balance: {:.2f} kr".format(float(info[3])))
                 print("  Has {}accepted EULA".format("" if info[4].strip() == "true" else "not "))
+
+
+def stats(args: List[str] = []):
+    if is_help(args):
+        print("Print statistics for users")
+        return
+
+    total_money = 0
+    total_user_debt = 0
+    users_in_debt = 0
+    total_users = 0
+
+    with open(userfile, "r") as f:
+        skipfirst = True
+
+        for line in f:
+            if skipfirst:
+                skipfirst = False
+                continue
+
+            info = line.split(",")
+
+            total_money += float(info[3])
+            total_user_debt -= float(info[3]) if float(info[3]) < 0 else 0
+            users_in_debt += float(info[3]) < 0
+            total_users += 1
+
+    print(f"Total users:    {total_users}")
+    print(f"Total balance:  {total_money:.2f} kr")
+    print(f"{users_in_debt} users are in debt, with a total of {total_user_debt:.2f} kr of debt")
+
+
